8e42ee65ab95d96ef9829aed3e084aaa702093ed
Yo Vannaravuth <ravuthz@gmail.com>
2017-04-14 16:08:25 +0000

    Generate API user

---
+++ api/controllers/UserController.js
@@ -0,0 +1,41 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.register()`
+   */
+  register: function (req, res) {
+    return res.json({
+      todo: 'register() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.login()`
+   */
+  login: function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.logout()`
+   */
+  logout: function (req, res) {
+    return res.json({
+      todo: 'logout() is not implemented yet!'
+    });
+  }
+};
+
